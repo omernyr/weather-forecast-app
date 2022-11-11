2a70f82de984e5547635a06e973698d62836be7f
Ömer NAYIR <omer.nayir.mdbf17@iste.edu.tr>
2022-11-11 14:43:22 +0300

    İcons and api added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,42 @@
 import './App.css';
+import Search from './components/search/search';
+import CurrentWeather from './components/current-weather/current-weather';
+import { WEATHER_API_KEY, WEATHER_API_URL } from "./api";
+import { useState } from 'react';
 
 function App() {
+
+  const [currentWeather, setCurrentWeather] = useState(null);
+  const [forecast, setForecast] = useState(null);
+
+  const handleOnSearchChange = (searchData) => {
+    const [lat, lon] = searchData.value.split(' ');
+
+    // Anlık hava verisi
+    const currentWeatherFetch = fetch(`${WEATHER_API_URL}/weather?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}`)
+    // Geçmiş 5 günlük 3 saatlik hava verisi
+    const forecastFetch = fetch(`${WEATHER_API_URL}/forecast?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}`)
+
+    Promise.all([currentWeatherFetch, forecastFetch])
+      .then(async (response) => {
+        const weatherResponse = await response[0].json();
+        const forecastResponse = await response[1].json();
+
+        setCurrentWeather({ city: searchData.label, ...weatherResponse });
+        setForecast({ city: searchData.label, ...forecastResponse });
+
+      })
+      .catch((err) => console.log(err))
+
+      console.log("Current Weather", currentWeather);
+      console.log("Forecast", forecast);
+
+  }
+
   return (
-    <div className="App">
-      test
+    <div className="container">
+      <Search onSearchChange={handleOnSearchChange} />
+      <CurrentWeather />
     </div>
   );
 }
